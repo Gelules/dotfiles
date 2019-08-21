2e2c8cc788894bbef41d9f6596eff291af1387bc
Jules Aubert <aubertjules@gmail.com>
2019-08-22 00:51:47 +0200

    awesome: Changed the rc.lua to have a nice wallpaper.

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -46,10 +46,11 @@ end
 -- {{{ Variable definitions
 -- Themes define colours, icons, font and wallpapers.
 beautiful.init(gears.filesystem.get_themes_dir() .. "default/theme.lua")
+beautiful.wallpaper = "/home/jules/.config/awesome/wallpaper-white.png"
 
 -- This is used later as the default terminal and editor to run.
 terminal = "urxvt"
-editor = os.getenv("EDITOR") or "nano"
+editor = os.getenv("EDITOR") or "vim"
 editor_cmd = terminal .. " -e " .. editor
 
 -- Default modkey.
